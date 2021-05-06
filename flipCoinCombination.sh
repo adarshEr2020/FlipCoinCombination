df8231d8b2e8a45054e3e809194f49f56183c981
adarshEr2020 <adarshprajapati31@gmail.com>
2021-05-06 19:12:17 +0530

    Add : singlet shows Heads or Tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
 echo "Welcome to Flip Coin Combination"
-flip=$(($RANDOM%2))
-if [ $flip -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+heads=0
+tails=0
+echo "Enter number of flip requried"
+read n
+for((i=0; i<n; i++))
+do
+	flip=$(($RANDOM%2))
+	if [ $flip -eq 1 ]
+	then
+		heads=$(($heads+1))
+	else
+		tails=$(($tails+1))
+	fi
+done
+echo "Number of head win :"$heads
+echo "Number od tail win :"$tails
