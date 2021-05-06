4317f2bbc04c6c962075597fe256a2c1072cd727
adarshEr2020 <adarshprajapati31@gmail.com>
2021-05-06 20:30:03 +0530

    flip coin triple combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -13,8 +13,8 @@ do
 	else
 		tails=$(($tails+1))
 	fi
-	flipp=$((RANDOM%4))
-	case $flipp in
+	flipD=$((RANDOM%4))
+	case $flipD in
 		0) hh=$(($hh+1))
 			;;
 		1) ht=$(($ht+1))
@@ -26,6 +26,27 @@ do
 		*) echo "went wrong"
 			;;
 	esac
+	flipT=$((RANDOM%7))
+	case $flipT in
+		0) hhh=$(($hhh+1))
+			;;
+		1) hht=$(($hht+1))
+			;;
+		2) hth=$(($hth+1))
+			;;
+		3) htt=$(($htt+1))
+			;;
+		4) thh=$(($thh+1))
+			;;
+		5) tht=$(($tht+1))
+			;;
+		6) tth=$(($tth+1))
+			;;
+		7) ttt=$(($ttt+1))
+			;;
+		*) echo "something went wrong"
+			;;
+	esac
 done
 echo "Number of head win :" $heads
 echo "Number of tail win :" $tails
@@ -34,3 +55,12 @@ echo "Number of ht combination " $ht
 echo "Number of th combination " $th
 echo "Number of tt combination " $tt
 
+echo "Number of hhh combination " $hhh
+echo "Number of hht combination " $hht
+echo "Number of hth combination " $hth
+echo "Number of htt combination " $htt
+
+echo "Number of thh combination " $thh
+echo "Number of tht combination " $tht
+echo "Number of tth combination " $tth
+echo "Number of ttt combination " $ttt
